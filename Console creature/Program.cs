456977074751ed9a878d4449af2e280057bf08cb
Jamagiae Medley <jamagiae.medley@outlook.com>
2020-09-29 13:49:53 -0400

    Add project files.

---
+++ Console creature/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Console_creature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Build a Creature
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| o |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("NinjaFive0");
+        }
+    }
+}
